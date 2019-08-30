f30099e96420211f31351eab73eb1b7ec31c7b95
Davi de Oliveira <davi.santos123@etec.sp.gov.br>
2019-08-29 22:12:30 -0300

    Dei inicio aos metodos Get e Set

---
+++ src/main/java/com/agenda/model/Contato.java
@@ -0,0 +1,29 @@
+package com.agenda.model;
+
+public class Contato {
+
+	
+	private long id;
+	private String tel;
+	private String email;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
--- src/main/java/com/agenda/model/Pessoa.java
+++ src/main/java/com/agenda/model/Pessoa.java
@@ -4,48 +4,34 @@ public class Pessoa {
 
 	private long id;
 	private String nome;
-	private String email;
-	private String telefone;
-	private String endereco;
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-
-	public String getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-
+	private String sexo;
+	private String nasc;
 	public long getId() {
 		return id;
 	}
-
 	public void setId(long id) {
 		this.id = id;
 	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getNasc() {
+		return nasc;
+	}
+	public void setNasc(String nasc) {
+		this.nasc = nasc;
+	}
+	
+
+	
 
 }
