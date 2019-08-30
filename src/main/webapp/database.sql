1c9ce639f33cb74e829a6fc011b944cd9a4261e5
Davi de Oliveira <davi.santos123@etec.sp.gov.br>
2019-08-29 21:41:45 -0300

    Criação do layout de cadastro

--- src/main/webapp/database.sql
+++ src/main/webapp/database.sql
@@ -1,8 +1,25 @@
+CREATE TABLE enderecos(
+	id_endereco INT NOT NULL AUTO_INCREMENT,
+	lougradouro_endereco 	VARCHAR(255),
+	cep_endereco 			CHAR(8),
+	PRIMARY KEY				(id_endereco)
+);
+
+CREATE TABLE contatos(
+	id_contato INT NOT NULL AUTO_INCREMENT,
+	email_contato 			VARCHAR(255),
+	telefone_contato 		VARCHAR(255),
+	PRIMARY KEY 	(id_contato)
+);
+
 CREATE TABLE pessoas (
-	id	BIGINT NOT	NULL AUTO_INCREMENT,
-	nome			VARCHAR(255),
-	email			VARCHAR(255),
-	endereco		VARCHAR(255),
-	telefone		VARCHAR(255),
-	PRIMARY KEY 	(id)
+	id_pessoa	INT NOT NULL AUTO_INCREMENT,
+	nome_pessoa			VARCHAR(255),
+	nascimento_pessoa	DATETIME, 
+	sexo_pessoa			CHAR(1),
+	fk_endereco			int,
+	fk_contato 			int,
+	PRIMARY KEY 		(id_pessoa),
+	FOREIGN KEY (fk_endereco) REFERENCES enderecos(id_endereco),
+	FOREIGN KEY (fk_contato) REFERENCES contatos(id_contato)
 );
